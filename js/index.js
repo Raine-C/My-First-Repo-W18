bed01b3d93a6efedd9fbe5ee20da44bef8e4a67b
Raine Cai <135162306+Raine-C@users.noreply.github.com>
2023-11-05 01:15:35 -0400

    20231105-0115
    javascript final

--- js/index.js
+++ js/index.js
@@ -1,14 +1,17 @@
 $(".TheatreArtcard").on("click", function () {
     var h = $('.TheatreArtcard').css('height');
-    var image = document.querySelector("#IMGR");
+    var image = document.querySelector("#IMGR1");
     console.log("test001");
 
     if (h == '465px') {
         $(".TheatreArtcard").animate({height: "560px"});
         image.style.transform = "rotate(180deg)"; 
         // $(".text").css("display", "none");
-        $(".text1").css("background-color", "rgba(27, 35, 33, 0)");
+        $(".text1").css("background-color", "rgba(33, 37, 36, 0)");
         $(".text1 h3").css("color", "#fafaef00");
+        document.querySelector(".text1").style.backdropFilter = "blur(0px)";
+
+
 
         console.log("test002");
 
@@ -17,8 +20,9 @@ $(".TheatreArtcard").on("click", function () {
     else {
         $(".TheatreArtcard").animate({height: "465px"});
         image.style.transform = "rotate(0deg)"; 
-        $(".text1").css("background-color", "rgba(27, 35, 33, 0.527)");
+        $(".text1").css("background-color", "rgba(33, 37, 36, 0.527)");
         $(".text1 h3").css("color", "#fafaef");
+        document.querySelector(".text1").style.backdropFilter = "blur(10px)";
         
     }
     
@@ -27,31 +31,41 @@ $(".TheatreArtcard").on("click", function () {
 
 $(".SalesEngineercard").on("click", function () {
    var h = $(".SalesEngineercard").css("height");
+   var image = document.querySelector("#IMGR2");
     
    if (h == "465px") {
     $(".SalesEngineercard").animate({height: "560px"});
-    $(".text2").css("background-color", "rgba(27, 35, 33, 0)");
+    image.style.transform = "rotate(180deg)"; 
+    $(".text2").css("background-color", "rgba(33, 37, 36, 0)");
     $(".text2 h3").css("color", "#fafaef00");
+    document.querySelector(".text2").style.backdropFilter = "blur(0px)";
    }
    else {
     $(".SalesEngineercard").animate({height: "465px"});
-    $(".text2").css("background-color", "rgba(27, 35, 33, 0.527)");
+    image.style.transform = "rotate(0deg)"; 
+    $(".text2").css("background-color", "rgba(33, 37, 36, 0.527)");
     $(".text2 h3").css("color", "#fafaef");
+    document.querySelector(".text2").style.backdropFilter = "blur(10px)";
    }
 
 });
 
 $(".ProjectManagercard").on("click", function () {
     var h = $(".ProjectManagercard").css("height");
+    var image = document.querySelector("#IMGR3");
     if (h == "465px"){
         $(".ProjectManagercard").animate({height: "560px"});
-        $(".text3").css("background-color", "rgba(27, 35, 33, 0)");
+        image.style.transform = "rotate(180deg)"; 
+        $(".text3").css("background-color", "rgba(33, 37, 36, 0)");
         $(".text3 h3").css("color", "#fafaef00");
+        document.querySelector(".text3").style.backdropFilter = "blur(0px)";
     }
     else{
         $(".ProjectManagercard").animate({height: "465px"});
-        $(".text3").css("background-color", "rgba(27, 35, 33, 0.527)");
+        image.style.transform = "rotate(0deg)"; 
+        $(".text3").css("background-color", "rgba(33, 37, 36, 0.527)");
         $(".text3 h3").css("color", "#fafaef");
+        document.querySelector(".text3").style.backdropFilter = "blur(10px)";
     }
    
 
